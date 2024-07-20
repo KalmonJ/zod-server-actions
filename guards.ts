1e671786402a65993ccd4d9248719416473baa8d
KalmonJ <kalmonkk69@gmail.com>
2024-07-19 22:17:45 -0300

    feat: add response error type guard

--- guards.ts
+++ guards.ts
@@ -1,3 +1,9 @@
+import { ResponseError } from "./types";
+
 export function isNullResponse(res: unknown): res is null {
   return res === null;
+}
+
+export function isResponseError(res: unknown): res is ResponseError {
+  return (res as ResponseError).error !== null;
 }
